d3c696a9436b3927d699ffe8907fde816a3f927d
watye <watye@qq.com>
2019-01-08 14:01:28 +0800

    Eureka Server安全认证配置

--- src/main/java/com/talelife/SpringcloudEurekaApplication.java
+++ src/main/java/com/talelife/SpringcloudEurekaApplication.java
@@ -1,4 +1,4 @@
-package com.talelife.springcloudeureka;
+package com.talelife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/talelife/config/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.talelife.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Eureka Server安全认证配置
+ *
+ * @author：lwy
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    /**
+     * 配置安全信息
+     * - 禁用csrf功能
+     * - 开启所有请求需要验证并且使用http basic进行认证
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
