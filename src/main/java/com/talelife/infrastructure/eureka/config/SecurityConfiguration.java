52b9c18979f9f5d3dafc491456502ea03c9e98d0
watye <watye@qq.com>
2020-05-08 14:16:27 +0800

    feat

--- src/main/java/com/talelife/infrastructure/eureka/config/SecurityConfiguration.java
+++ src/main/java/com/talelife/infrastructure/eureka/config/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.talelife.config;
+package com.talelife.infrastructure.eureka.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
